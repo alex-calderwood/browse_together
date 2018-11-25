fbfe72353b601a7e6e8775ff1bb37cf8ab140079
Alex <alexander.d.calderwood@gmail.com>
2018-11-25 00:22:22 -0500

    Finished first go at sidebar for groups.

--- static/group.js
+++ static/group.js
@@ -7,13 +7,10 @@ $(document).ready(function() {
 
     //  Receive a message
     socket.on('message', function(msg){
-//        console.log('got html message' +  msg)
 
         m = msg.split('~')
         var id_num = m[0]
         var html = m[1]
-
-//        console.log(id_num, html)
 
         table_to_append = "#" + id_num + "-messages"
         console.log('appending to', table_to_append)
@@ -28,6 +25,11 @@ $(document).ready(function() {
         myMessage = myMessage + '~' + user
         socket.send(myMessage)
         console.log('Sent message', myMessage)
+    })
+
+    // Collapse sidebar
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
     })
 
 //    console.log('alex', document.getElementById("my-frame").contentWindow.location.href)
