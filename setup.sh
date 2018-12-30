3d99cd810912075e11fb8b550d8daf0194001f2c
Alex Calderwood <alexander.d.calderwood@gmail.com>
2018-12-30 16:52:01 -0500

    Updated setup scripts.

---
+++ setup.sh
@@ -0,0 +1,24 @@
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+
+sudo apt-get install xvfb
+
+sudo apt-get install unzip
+
+wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
+sudo sapt-get install -y libglib2.0-0=2.50.3-2 
+    libnss3=2:3.26.2-1.1+deb9u1 \
+    libgconf-2-4=3.2.6-4+b1 \
+    libfontconfig1=2.11.0-6.7+b1
+
+pip install -r requirements.txt
